feb243562a95c75c9f57e3a45a0ec652c421762a
Hafsasiddiqui27 <hafsasiddiqui27@gmail.com>
2024-11-11 05:23:29 -0800

    nav bar align

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -8,7 +8,7 @@ import { Menu } from 'lucide-react';
 const Header = () => {
   return (
     <Wrapper>
-    <header className='flex justify-between bg-white py-6 sticky top-0 items-center' >
+    <header className='flex justify-between items-center bg-inherit py-6 sticky top-0' >
         {/* logo */}
         <div>
         <h2 className='text-teal-400 text-2xl font-bold font-serif'>PhysicalTherapy Center</h2>
@@ -16,20 +16,21 @@ const Header = () => {
         {/* navigation bar */}
       
         <ul className='hidden md:block'>
-            <li className='flex space-x-6'>
+      <li className='flex space-x-6'>
             <Link className='hover:font-semibold' href='/'>Home</Link>
             <Link className='hover:font-semibold' href='/about'>About</Link>
             <Link className='hover:font-semibold' href='/contact'>Contact Us</Link>
-            </li>
+           </li>
         </ul>
+      
  
     <Sheet>
-  <SheetTrigger><Menu className='block md:hidden bg-white'/></SheetTrigger>
+  <SheetTrigger className='md:hidden bg-white'><Menu /></SheetTrigger>
   <SheetContent className='bg-slate-200 w-fit'>
-  <div>
+        <div>
         <h2 className='text-teal-400 text-2xl font-bold font-serif'>PhysicalTherapy Center</h2>
         </div>
-  <ul className='flex-col'>
+       <ul className='flex-col'>
              
             <li> <Link className='hover:font-semibold m-2' href='/'>Home</Link></li>
             <li><Link className='hover:font-semibold  m-2' href='/about'>About</Link></li> 
